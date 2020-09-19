d419dc04e13d5ad5581f0160b0b1f271df1bc52d
Sergey Minkov <usverger@Sergeys-MacBook-Pro.local>
2020-09-19 12:57:43 -0400

    binary tree recursive

--- python/tests/testTrees.py
+++ python/tests/testTrees.py
@@ -5,18 +5,39 @@ import python.problems as problems
 class Trees(unittest.TestCase):
 
     def test_sumofRootToLeafBinaryNumbers(self):
-        s = problems.SumofRootToLeafBinaryNumbers.Solution()
+        s = problems.BinaryTree.Solution()
         treeNode = s.generateTreeNode(None, 0, [1,0,1,0,1,0,1])
         
         r = s.sumRootToLeaf(treeNode)
         self.assertEqual(r, 22, 22)
 
-    def test_maxDepth(self):
-        s = problems.MaximumDepthOfBinaryTree.Solution()
+    def test_maxDepthIteration(self):
+        s = problems.BinaryTree.Solution()
         treeNode = s.generateTreeNode(None, 0, [3,9,20,None,None,15,7])
         
-        r = s.maxDepth(treeNode)
+        r = s.maxDepthIteration(treeNode)
         self.assertEqual(r, 3, 3)
+
+    def test_maxDepthRecursive(self):
+        s = problems.BinaryTree.Solution()
+        treeNode = s.generateTreeNode(None, 0, [3,9,20,None,None,15,7])
+        
+        r = s.maxDepthRecursive(treeNode)
+        self.assertEqual(r, 3, 3)
+
+    def test_maxDepthRecursiveEmpty(self):
+        s = problems.BinaryTree.Solution()
+        treeNode = s.generateTreeNode(None, 0, [])
+        
+        r = s.maxDepthRecursive(treeNode)
+        self.assertEqual(r, 0, 0)
+
+    def test_minDepth(self):
+        s = problems.BinaryTree.Solution()
+        treeNode = s.generateTreeNode(None, 0, [3,9,20,None,None,15,7])
+        
+        r = s.minDepth(treeNode)
+        self.assertEqual(r, 2, 2)
 
 
  
