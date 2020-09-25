8688a5ca111463032aa01386e389d8d62b1286c3
Sergey Minkov <usverger@gmail.com>
2020-09-25 00:36:52 -0400

    https://leetcode.com/problems/largest-triangle-area

--- python/problems/LargestTriangleArea.py
+++ python/problems/LargestTriangleArea.py
@@ -5,6 +5,12 @@ import itertools
 class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
         
+        def combinationsOfThree(arr: List[int]) -> List[List[int]]:
+            for i in range(len(arr) - 2):
+                for j in range(i + 1, len(arr) - 1):
+                    for k in range(j + 1, len(arr)):
+                        yield (arr[i], arr[j], arr[k])
+
         def area(a: List[int], b: List[int], c:List[int]) -> float:
             
             # area is determinant of matrix divided by 2, absolute value
@@ -12,11 +18,16 @@ class Solution:
             #               | x2 - x3   y2 - y3 |
             
             return 0.5 * abs(  (a[0] - c[0]) * (b[1] - c[1]) - (a[1] - c[1]) * (b[0] - c[0])  )
+    
+        # just for the sake of comparing the implementation
+        arr = [1,2,3,4,5]
+        print(list(itertools.combinations(arr, 3)))
+        print(list(combinationsOfThree(arr)))
         
         # enumerate all possible combinations by 3 points
-        combinations = itertools.combinations(points, 3)
+        iterator = itertools.combinations(points, 3)
         # return max
-        return max([area(c[0], c[1], c[2]) for c in combinations])
+        return max([area(c[0], c[1], c[2]) for c in iterator])
                     
                 
             
