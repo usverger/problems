9d3706322b89442a00c63f02ac38e9a9bb3e0321
Sergey Minkov <usverger@gmail.com>
2020-11-08 16:41:25 -0500

    https://leetcode.com/problems/shuffle-an-array/

--- python/problems/DesignSuffleArray.py
+++ python/problems/DesignSuffleArray.py
@@ -19,28 +19,3 @@ class Solution:
         Returns a random shuffling of the array.
         """
         return sorted(self.nums, key = lambda x: random.random())
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.reset()
-# param_2 = obj.shuffle()
-
-    def __init__(self):
-        self.l = []
-        
-    def push(self, x: int) -> None:
-        if self.l:
-            m = self.l[-1][1]
-            self.l.append([x, min(x,m)])
-        else:
-            self.l.append([x, x])
-
-    def pop(self) -> None:
-        self.l.pop()
-
-    def top(self) -> int:
-        return self.l[-1][0]
-
-    def getMin(self) -> int:
-        return self.l[-1][1]
